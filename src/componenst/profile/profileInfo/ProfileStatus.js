d709745355377b62dc1de31ebbd1abdfba9d9db0
DenisPr-SPb <denispr89@gmail.com>
2023-05-30 13:17:58 +0300

    Add status profile component

--- src/componenst/profile/profileInfo/ProfileStatus.js
+++ src/componenst/profile/profileInfo/ProfileStatus.js
@@ -1,12 +1,37 @@
 import React from "react";
 
-export default function ProfileStatus ({status}) {
-    return (
-        <div className="profile__status__wrapper">
-            <div className="profile__status">
-                <div className="status__text">{status}</div>
-                <input type="text" className="status__input" placeholder='your status' value={status}/>
+class ProfileStatus extends React.Component {
+
+    state = {
+        editMode: false
+    }
+
+    activateEditMode = () => {
+        this.setState( {
+            editMode: true
+        } )
+    }
+
+    deactivateEditMode = () => {
+        this.setState( {
+            editMode: false
+        } )
+    }
+
+    render() {
+        return (
+            <div className="profile__status__wrapper">
+                <div className="profile__status">
+                    {!this.state.editMode &&
+                        <div onDoubleClick={ () =>  { this.activateEditMode() }} className="status__text">{this.props.status}</div>}
+
+                    {this.state.editMode &&
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} type="text" className="status__input" placeholder='your status'
+                               value={this.props.status}/>}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default ProfileStatus
